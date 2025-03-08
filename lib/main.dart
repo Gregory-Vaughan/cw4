2e1dcf28b336d9700bd1f400dee11e431c474101
Gregory-Vaughan <gpv520@gmail.com>
2025-03-07 19:21:33 -0500

    3rd commit, added Plan class into code.

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,25 @@ class AdoptionTravelPlannerApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Adoption & Travel Planner',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const PlanManagerScreen(),
     );
   }
+}
+
+
+class Plan {
+  String name;
+  String description;
+  DateTime date;
+  bool isCompleted;
+
+  Plan({
+    required this.name,
+    required this.description,
+    required this.date,
+    this.isCompleted = false,
+  });
 }
 
 class PlanManagerScreen extends StatefulWidget {
@@ -28,7 +41,7 @@ class PlanManagerScreen extends StatefulWidget {
 }
 
 class _PlanManagerScreenState extends State<PlanManagerScreen> {
-  final List<String> plans = [];
+  final List<Plan> plans = [];
 
   @override
   Widget build(BuildContext context) {
@@ -39,23 +52,31 @@ class _PlanManagerScreenState extends State<PlanManagerScreen> {
           : ListView.builder(
               itemCount: plans.length,
               itemBuilder: (context, index) {
+                final plan = plans[index];
                 return ListTile(
-                  title: Text(plans[index]),
-                  leading: const Icon(Icons.event_note),
+                  title: Text(plan.name),
+                  subtitle: Text(plan.description),
+                  trailing: Icon(
+                    plan.isCompleted ? Icons.check_circle : Icons.radio_button_unchecked,
+                    color: plan.isCompleted ? Colors.green : Colors.grey,
+                  ),
                 );
               },
             ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _createPlan,
+        onPressed: _createSamplePlan,
         child: const Icon(Icons.add),
       ),
     );
   }
 
-  void _createPlan() {
-    
+  void _createSamplePlan() {
     setState(() {
-      plans.add('New Plan ${plans.length + 1}');
+      plans.add(Plan(
+        name: 'Sample Plan ${plans.length + 1}',
+        description: 'Description for plan ${plans.length + 1}',
+        date: DateTime.now(),
+      ));
     });
   }
 }
